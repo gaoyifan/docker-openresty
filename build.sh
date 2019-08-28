e4f95d6e22ddf629139340f4ff27f71c938c5a3f
Yifan Gao <git@gaoyifan.com>
2019-08-28 13:51:32 +0800

    Add light server image

---
+++ build.sh
@@ -0,0 +1,9 @@
+docker build \
+    --build-arg=RESTY_J=16 \
+    -f alpine/Dockerfile \
+    -t gaoyifan/openresty:light .
+docker build \
+    --build-arg=RESTY_IMAGE_BASE=gaoyifan/openresty \
+    --build-arg=RESTY_IMAGE_TAG=light \
+    -f alpine/Dockerfile.fat \
+    -t gaoyifan/openresty:light-fat .
