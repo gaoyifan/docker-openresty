4f496608f529347c8046c9fc415f30c31309185b
Yifan Gao <git@yfgao.com>
2019-09-01 19:39:02 +0800

    Add nginx-http-radius-module

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 docker build \
     --build-arg=RESTY_J=16 \
     -f alpine/Dockerfile \
-    -t gaoyifan/openresty:light .
+    -t gaoyifan/openresty:light-radius .
 docker build \
     --build-arg=RESTY_IMAGE_BASE=gaoyifan/openresty \
     --build-arg=RESTY_IMAGE_TAG=light \
     -f alpine/Dockerfile.fat \
-    -t gaoyifan/openresty:light-fat .
+    -t gaoyifan/openresty:light-fat-radius .
